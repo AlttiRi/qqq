75bbd7de98946553121c6cd5fa29a2931573fa2e
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2020-06-27 16:26:55 +0300

    "Update"

--- service-worker.js
+++ service-worker.js
@@ -11,29 +11,29 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', async function(event) {
     console.log('👷', 'fetch', event);
 
-    if (event.request.method !== 'POST') {
-        //event.respondWith(fetch(event.request));
-        return;
-    }
-
-    // const formData = await event.request.formData();
-    // const text = formData.get('text') || '';
-    // alert(text);
-    // let a = "hhh";
-    // const blob = new Blob([`<!DOCTYPE html><html lang="en"><h1>${a}</h1></html>`],{type: "text/html"});
-    // event.respondWith(new Response("blob"));
-
-
-    event.respondWith((async () => {
-        const formData = await event.request.formData();
-        const text = formData.get('text') || '';
-        // const media = formData.get('media') || '';
-
-
-
-        // return new Response(event.request.url + `?text=${encodeURIComponent(text)}`);
-        // return Response.redirect(event.request.url + `?text=${text}`, 303);
-        return fetch(event.request.url + `?text=${encodeURI(text)}`); // bad
-    })());
+    // if (event.request.method !== 'POST') {
+    //     //event.respondWith(fetch(event.request));
+    //     return;
+    // }
+    //
+    // // const formData = await event.request.formData();
+    // // const text = formData.get('text') || '';
+    // // alert(text);
+    // // let a = "hhh";
+    // // const blob = new Blob([`<!DOCTYPE html><html lang="en"><h1>${a}</h1></html>`],{type: "text/html"});
+    // // event.respondWith(new Response("blob"));
+    //
+    //
+    // event.respondWith((async () => {
+    //     const formData = await event.request.formData();
+    //     const text = formData.get('text') || '';
+    //     // const media = formData.get('media') || '';
+    //
+    //
+    //
+    //     // return new Response(event.request.url + `?text=${encodeURIComponent(text)}`);
+    //     return Response.redirect(event.request.url + `?text=${text}`, 303);
+    //     // return fetch(event.request.url + `?text=${encodeURI(text)}`); // bad
+    // })());
 
 });
