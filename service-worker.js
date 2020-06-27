8ed89157393a4d6ce2166c4d97e1105686822936
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2020-06-27 12:20:50 +0300

    Update

--- service-worker.js
+++ service-worker.js
@@ -14,12 +14,4 @@ self.addEventListener('fetch', function(event) {
     const blob = new Blob([`<!DOCTYPE html><html lang="en"><h1>5555</h1></html>`],{type: "text/html"});
     event.respondWith(new Response(blob));
 
-    // event.respondWith(new Response(
-    //     `<!DOCTYPE html><html lang="en"><h1>1234567</h1></html>`,
-    //     {
-    //         status: 200,
-    //         headers: {
-    //             "Content-Type": "text/html; charset=utf-8"
-    //         }
-    //     }));
 });
