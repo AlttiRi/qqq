5a02fe79373cb3ceb08666a1542daecde46840d6
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2020-06-27 19:31:03 +0300

    Sync

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,17 @@ self.addEventListener('fetch', async function(event) {
 
     if (event.request.method !== 'POST') {
         //event.respondWith(fetch(event.request));
-        return;
+        // return;
+
+        event.respondWith((async () => {
+            // Get content from the network.
+            try {
+                return await fetch(event.request);
+            } catch (e) {
+                // Failure. Just return a 200 page, to satisfy Lighthouse.
+                return new Response('You are offline', {status: 200});
+            }
+        })());
     }
 
     // const formData = await event.request.formData();
