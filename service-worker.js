462b88ea072f21f0eea5b0d7016272f59e81a219
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2020-06-27 15:06:48 +0300

    "Update"

--- service-worker.js
+++ service-worker.js
@@ -12,13 +12,15 @@ self.addEventListener('fetch', async function(event) {
     console.log('👷', 'fetch', event);
 
     if (event.request.method !== 'POST') {
-        event.respondWith(fetch(event.request));
+        //event.respondWith(fetch(event.request));
         return;
     }
 
     const formData = await event.request.formData();
     const text = formData.get('text') || '';
-    const blob = new Blob([`<!DOCTYPE html><html lang="en"><h1>${text}</h1></html>`],{type: "text/html"});
+    alert(text);
+    let a = "hhh";
+    const blob = new Blob([`<!DOCTYPE html><html lang="en"><h1>${a}</h1></html>`],{type: "text/html"});
     event.respondWith(new Response(blob));
 
 
