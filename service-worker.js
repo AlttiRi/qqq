87b731614b2c000a918cd1b16c4b33c6f6209882
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2020-06-28 12:08:02 +0300

    Sync

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,42 @@
-self.addEventListener('install', (event) => {
-  console.log('👷', 'install', event);
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('👷', 'activate', event);
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', async function(event) {
-    console.log('👷', 'fetch', event);
-
-    if (event.request.method !== "POST") {
-        event.respondWith((async () => {
-            try {
-                return await fetch(event.request);
-            } catch (e) {
-                const html =
+const offlineHtml =
 `<!DOCTYPE html>
 <html lang="en">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <h1>You are offline</h1>
 </html>`;
-                const blob = new Blob([html],{type: "text/html"});
+
+
+
+self.addEventListener("install", (event) => {
+  console.log("👷", "install", event);
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("👷", "activate", event);
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", async function(event) {
+    console.log("👷", "fetch", event);
+
+    if (event.request.method !== "POST") {
+        return event.respondWith((async () => {
+            try {
+                return await fetch(event.request);
+            } catch (e) {
+                const blob = new Blob([offlineHtml],{type: "text/html"});
                 return new Response(blob, {status: 200});
             }
         })());
     }
 
 
-    event.respondWith((async () => {
+    return event.respondWith((async () => {
         const formData = await event.request.formData();
-        const text = formData.get('text') || '';
-        // const media = formData.get('media') || '';
-        const mediaFiles = formData.getAll('media') || [];
+        const text = formData.get("text") || "";
+        // const media = formData.get("media") || "";
+        const mediaFiles = formData.getAll("media") || [];
         const files = mediaFiles.map(file => JSON.stringify({
             name: file.name,
             mtime: file.lastModified,
