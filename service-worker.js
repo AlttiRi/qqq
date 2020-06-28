85fd244e463b8399569229ebfd80c9ed9e0ffc11
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2020-06-28 13:17:55 +0300

    Sync

--- service-worker.js
+++ service-worker.js
@@ -38,7 +38,7 @@ self.addEventListener("fetch", async function(event) {
         const title = formData.get("title") || "";
         const url = formData.get("url") || "";
 
-        const mediaFiles = formData.getAll("images_") || [];
+        const mediaFiles = formData.getAll("medias_") || [];
         const files = mediaFiles.map(file => JSON.stringify({
             name: file.name,
             mtime: file.lastModified,
