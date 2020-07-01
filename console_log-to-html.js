40c0e63ef9a86c5f50d94509f6c3ab6a5d89770d
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2020-07-01 16:27:00 +0300

    Sync

--- console_log-to-html.js
+++ console_log-to-html.js
@@ -14,7 +14,6 @@ function getHtmlLog(func, className) {
         }
 
         for (const argument of arguments) {
-
             const str = toString(argument);
 
             const block = document.createElement("pre");
@@ -36,35 +35,6 @@ function toString(argument) {
 
     try {
         function replacer(key, value) {
-            if (key === "parent") { // to resolve the circular structure
-                return undefined;
-            }
-            if (ArrayBuffer.isView(value)) {
-                const name = value[Symbol.toStringTag] ?? value?.constructor.name;
-
-                const array = [`${name}(${value.length}):`];
-                let subArray = [];
-                const max = 256;
-
-                let i = 0;
-                for (let elem of [...value.subarray(0, max)]) {
-                    subArray.push(elem.toString().padStart(4));
-                    i++;
-                    if (i % 16 === 0) {
-                        array.push(subArray.join(","));
-                        subArray = [];
-                    }
-                }
-                if (subArray.length > 0) {
-                    array.push(subArray.join(","));
-                }
-                if (value.length > max) {
-                    array.push("...");
-                }
-
-                //return `${name}([${array.join("\n")}])`;
-                return array;
-            }
             return value;
         }
         return JSON.stringify(argument, replacer, "  ");
